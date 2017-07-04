5813fcd9c6b499c4c2a6eaf4560f72e3ed138330
NyondoPaul <nyondopaul23@gmail.com>
2017-07-04 11:04:40 +0300

    add doctor class tests

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 """ Tests for hospital oop design """
 import unittest
-from hospital import Person
+from hospital import Person, Patient , Doctor 
 
 class TestPerson(unittest.TestCase):
     """ tests for the Person class """
@@ -11,5 +11,12 @@ class TestPerson(unittest.TestCase):
         """ test the __init__ method """
         self.assertEqual([self.person.first_name, self.person.last_name,
         self.person.sex],['Paul','Nyondo','M'])
+
+class TestDoctor(unittest.TestCase):
+    def setUp(self):
+        self.doc = Doctor('Morgan', 'Freeman', 'M')
+    def testdoc(self):
+        self.assertEqual(self.doc.first_name, self.doc.last_name,
+        self.doc.sex,['Morgan','Freeman','M'])
 
 unittest.main()
