3009fc36b97183a2c7ced8b6fbceb0cedbe7862b
NyondoPaul <nyondopaul23@gmail.com>
2017-07-04 12:56:20 +0300

    Add discharge, treat methods

--- hospital.py
+++ hospital.py
@@ -18,6 +18,11 @@ class Doctor(Person):
         """initialise the doctor class """
         super(Doctor, self).__init__(first_name, last_name, sex)
 
+    def treat(self):
+        """ change sick from true to false"""
+        return False
+
+
 class Hospital(object):
     def __init__(self, name):
         self.name = name
@@ -35,8 +40,25 @@ class Hospital(object):
     def add_patient(self,first_name, last_name, sex):
         """ check in patients """
         pat = Patient(first_name, last_name, sex)
-        self.doctors[self.num_of_patients] = pat
+        self.patients[self.num_of_patients] = pat
         self.num_of_patients +=1
+
+    def treat(self, doctor, patient):
+        """ Function that gets a doctor to treat patient"""
+        doc = self.doctors[doctor]
+        self.patients[patient].sick = doc.treat()
+
+    def discharge(self):
+        """ Discharges cured patients """
+        discharged = []
+        for patient in self.patients:
+            if self.patients[patient].sick:
+                pass
+            else:
+                discharged.append(patient)
+        for patient in discharged:
+            self.patients.pop(patient)
+
 
 
 
