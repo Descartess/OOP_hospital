bbce39869c1c5bd4ab559eedee9cc2ca011a21d2
NyondoPaul <nyondopaul23@gmail.com>
2017-07-04 11:08:08 +0300

    add patient  class tests

--- tests.py
+++ tests.py
@@ -13,10 +13,21 @@ class TestPerson(unittest.TestCase):
         self.person.sex],['Paul','Nyondo','M'])
 
 class TestDoctor(unittest.TestCase):
+     """ tests for the Doctor class """
     def setUp(self):
         self.doc = Doctor('Morgan', 'Freeman', 'M')
     def testdoc(self):
         self.assertEqual(self.doc.first_name, self.doc.last_name,
         self.doc.sex,['Morgan','Freeman','M'])
+
+class TestPatient(unittest.TestCase):
+     """ tests for the Patient class """
+    def setUp(self):
+        self.patient = Patient('Morgan', 'Freeman', 'M')
+    def testpatient(self):
+        self.assertEqual(self.doc.first_name, self.doc.last_name,
+        self.doc.sex,['Morgan','Freeman','M'])
+    def testssickeness(self):
+        self.assertTrue(self.sick)
 
 unittest.main()
