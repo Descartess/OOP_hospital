93553f275cd57ff0e98b231b603ed21c3a06f438
NyondoPaul <nyondopaul23@gmail.com>
2017-07-04 12:01:27 +0300

    Add patient and doctor methods to hospital class

--- hospital.py
+++ hospital.py
@@ -17,4 +17,27 @@ class Doctor(Person):
     def __init__(self, first_name, last_name, sex):
         """initialise the doctor class """
         super(Doctor, self).__init__(first_name, last_name, sex)
+
+class Hospital(object):
+    def __init__(self, name):
+        self.name = name
+        self.num_of_docs = 0
+        self.num_of_patients = 0
+        self.doctors = {}
+        self.patients = {}
+
+    def add_doctor(self,first_name, last_name, sex):
+        """ hire doctors """
+        doc = Doctor(first_name, last_name, sex)
+        self.doctors[self.num_of_docs] = doc
+        self.num_of_docs +=1
+
+    def add_patient(self,first_name, last_name, sex):
+        """ check in patients """
+        pat = Patient(first_name, last_name, sex)
+        self.doctors[self.num_of_patients] = pat
+        self.num_of_patients +=1
+
+
+
     
