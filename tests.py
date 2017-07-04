8bafc06c5bcf5320a69b4926b844bae5ef39baac
NyondoPaul <nyondopaul23@gmail.com>
2017-07-04 11:39:34 +0300

    Add docs to functions

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 """ Tests for hospital oop design """
 import unittest
-from hospital import Person, Patient , Doctor 
+from hospital import Person, Patient, Doctor
 
 class TestPerson(unittest.TestCase):
     """ tests for the Person class """
@@ -17,6 +17,7 @@ class TestDoctor(unittest.TestCase):
     def setUp(self):
         self.doc = Doctor('Morgan', 'Freeman', 'M')
     def testdoc(self):
+        """ tests for the __init__ method """
         self.assertEqual([self.doc.first_name, self.doc.last_name,
         self.doc.sex],['Morgan','Freeman','M'])
 
@@ -25,9 +26,11 @@ class TestPatient(unittest.TestCase):
     def setUp(self):
         self.patient = Patient('Jon', 'Snow', 'M')
     def testpatient(self):
+        """ tests for the __init__ method """
         self.assertEqual([self.patient.first_name, self.patient.last_name,
         self.patient.sex],['Jon','Snow','M'])
     def testssickeness(self):
+        """ checks if admitted patient is sick"""
         self.assertTrue(self.patient.sick)
 
 unittest.main()
